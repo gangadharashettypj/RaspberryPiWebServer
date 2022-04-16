005115c4588c47c3296a6fd6237207eea2903173
Gangadhara Shetty P J <gangadharashetty.gs@gmail.com>
2022-04-16 15:32:22 +0530

    inital commit

---
+++ bananna_training.py
@@ -0,0 +1,29 @@
+import cv2
+from detecto.core import Model
+
+model = Model.load('model_weights_final.pth',
+                   ['Banana Bacterial Wilt', 'Black sigatoka disease', 'Healthy'])
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  # set Width
+cap.set(4, 480)  # set Height
+ret, img = cap.read()
+image = cv2.flip(img, 1)
+labels, boxes, scores = model.predict(image)
+predictions = model.predict(image)
+
+labels, boxes, scores = predictions
+print(labels[0])
+print(scores[0])
+
+cv2.imshow('image', image)
+
+
+while True:
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:  # press 'ESC' to quit
+        break
+
+cv2.destroyAllWindows()
+
